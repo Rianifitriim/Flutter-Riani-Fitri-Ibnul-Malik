06fe5054a36735cdf8157c9e50fd75d2164c8711
Rianifitriim <59384629+Rianifitriim@users.noreply.github.com>
2022-03-16 15:11:38 +0700

    Add files via upload

---
+++ 11_Introduction Flutter Widget/pratikum/soal1&soal2.dart
@@ -0,0 +1,168 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Widget',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Widget'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int seconds = 0;
+  int minutes = 0;
+  int hours = 0;
+
+  void detik() async {
+    for (int detik = 1; detik <= 60; detik++) {
+      await Future.delayed(const Duration(seconds: 1), () {
+        setState(() {
+          seconds++;
+        });
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    detik();
+  }
+
+//soal 1 membuat statefull widget membuat konten berubah tiap detik
+  @override
+  Widget build(BuildContext context) {
+    if (seconds == 60) {
+      setState(() {
+        seconds = 0;
+        minutes++;
+      });
+      if (seconds == 0) {
+        detik();
+      }
+    }
+
+    if (minutes == 60) {
+      setState(() {
+        minutes = 0;
+        hours++;
+      });
+    }
+
+    if (hours == 23) {
+      setState(() {
+        hours = 0;
+      });
+    }
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+//soal 2 membuat extract widget dapat digunakan kembali tetapi tidak mengubah tampilannya
+                hour(hours: hours),
+                colon(),
+                minute(minutes: minutes),
+                colon(),
+                second(seconds: seconds),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class second extends StatelessWidget {
+  const second({
+    Key? key,
+    required this.seconds,
+  }) : super(key: key);
+
+  final int seconds;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      seconds > 9 ? '$seconds' : '0$seconds',
+      style: Theme.of(context).textTheme.headline2,
+    );
+  }
+}
+
+class minute extends StatelessWidget {
+  const minute({
+    Key? key,
+    required this.minutes,
+  }) : super(key: key);
+
+  final int minutes;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      minutes > 9 ? '$minutes' : '0$minutes',
+      style: Theme.of(context).textTheme.headline2,
+    );
+  }
+}
+
+class colon extends StatelessWidget {
+  const colon({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      ':',
+      style: Theme.of(context).textTheme.headline2,
+    );
+  }
+}
+
+class hour extends StatelessWidget {
+  const hour({
+    Key? key,
+    required this.hours,
+  }) : super(key: key);
+
+  final int hours;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      hours > 9 ? '$hours' : '0$hours',
+      style: Theme.of(context).textTheme.headline2,
+    );
+  }
+}
---
+++ 11_Introduction Flutter Widget/summary.md
@@ -0,0 +1,34 @@
+# Introduction Flutter Widget
+
+## Resume
+Dalam materi ini, mempelajari:
+1. Pengenalan Flutter
+2. Stateless Widget
+3. Stateful Widget
+4. Built in Widget
+
+
+
+## Pengenalan Flutter
+Flutter adalah sebuah framework aplikasi mobil sumber terbuka yang diciptakan oleh Google. Flutter digunakan dalam pengembangan aplikasi untuk sistem operasi Android, iOS, Windows, Linux, MacOS, serta menjadi metode utama untuk membuat aplikasi Google Fuchsia
+
+## Stateless widget
+Stateless widget merupakan suatu widget yang dimuat secara statis dimana seluruh konfigurasi yang dimuat didalamnya telah diinisiasikan sejak awal widget tersebut dimuat.
+
+## Stateful widget
+ Stateful widget dapat mengubah atau mengupdate tampilan, menambah widget laiinya, mengubah nilai variabel, icon, warna dan masih banyak lagi.
+
+## Abstract Class
+Widget yang dapat langsung digunakan atau sudah terinstall di flutter
+
+
+## Task
+### Soal 1 & 2 Membuat class extends dan overide
+Output soal 1 dan 2 sebagai berikut
+
+![soal1 2](https://user-images.githubusercontent.com/59384629/157628433-1df3f431-e09e-4af7-8b02-546a4b802dc7.png)
+
+
+
+## 2. Soal 3 & 4 Membuat class extends dan implements
+Output soal 3 dan 4 sebagai berikut
