332e382f026d5bacede8989a17f94f88df9e45d8
Rianifitriim <59384629+Rianifitriim@users.noreply.github.com>
2022-03-28 15:47:42 +0700

    Add files via upload

---
+++ 16_Form Input Button/pratikum/soal1&2/main.dart
@@ -0,0 +1,22 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'home_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'JSON Listview in Flutter',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Homepage(),
+    );
+  }
+}
---
+++ 16_Form Input Button/summary.md
@@ -0,0 +1,28 @@
+# 14_Flutter Layout
+
+## Resume
+Dalam materi ini, mempelajari:
+1. Form
+2. Input
+3. Button
+
+
+
+## Form
+Form adalah tempat pengumpulan data inputan dari pengguna
+
+## Input
+Input adalah untuk memasukkan/menerima data dari pengguna
+
+## Button
+Button adalah untuk memberikan aksi/widget lain ketika ditekan
+
+
+## Task
+### Soal 1 membuat page contact dan button add
+Output soal 1
+
+
+
+### 2. Soal 2 membuat page new contact dan form submit
+Output soal 2 
