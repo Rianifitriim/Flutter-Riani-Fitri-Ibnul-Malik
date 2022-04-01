2867a0e0a708356da5c0aa9f77c4a4838a50ebcc
Rianifitriim <59384629+Rianifitriim@users.noreply.github.com>
2022-04-01 14:25:10 +0700

    Add files via upload

---
+++ 19_Flutter Navigation/pratikum/new_contact.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+class newContact extends StatefulWidget {
+  newContact({Key? key}) : super(key: key);
+
+  @override
+  State<newContact> createState() => _newContactState();
+}
+
+class _newContactState extends State<newContact> {
+  var formKey = GlobalKey<FormState>();
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+            backgroundColor: Color.fromARGB(255, 236, 187, 245),
+            appBar: AppBar(
+              title: Text('New Contacts'),
+              backgroundColor: Colors.purpleAccent.shade400,
+            ),
+            body: Form(
+              key: formKey,
+              child: Container(
+                  padding: EdgeInsets.all(20),
+                  child: Column(
+                    children: [
+                      Column(
+                        children: [
+                          Container(
+                            margin: EdgeInsets.only(bottom: 20),
+                            child: TextFormField(
+                              decoration: new InputDecoration(
+                                hintText: "masukan nama lengkap anda",
+                                labelText: "Nama Lengkap",
+                                icon: Icon(Icons.people),
+                                border: OutlineInputBorder(
+                                    borderRadius:
+                                        new BorderRadius.circular(5.0)),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      TextFormField(
+                        decoration: new InputDecoration(
+                          hintText: "masukan nomor telepon anda",
+                          labelText: "No_hp",
+                          icon: Icon(Icons.contact_phone),
+                          border: OutlineInputBorder(
+                              borderRadius: new BorderRadius.circular(5.0)),
+                        ),
+                      ),
+                      ElevatedButton(
+                        onPressed: () {},
+                        child: Text('Submit'),
+                        style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all(
+                              Colors.purpleAccent.shade400),
+                        ),
+                      )
+                    ],
+                  )),
+            )));
+  }
+}
---
+++ 19_Flutter Navigation/summary.md
@@ -0,0 +1,31 @@
+# 19_Flutter Navigation
+
+## Resume
+Dalam materi ini, mempelajari:
+1. Pengertian Navigation
+2. Navigation Dasar
+3. Navigation Named Routes
+
+
+
+## Navigation
+Navigation berfungsi untuk berpindah ke halaman lain
+
+## Input
+Navigation dasar berpindah menggunakan navigation push tanpa menggunakan named routes
+
+## Button
+Navigation named routes setiap halaman memiliki nama masing-masing atau alamat
+
+
+## Task
+### Soal 1 membuat page contact dan new contact
+Output soal 1
+
+![soal1](https://user-images.githubusercontent.com/59384629/160361428-e4b5408b-0c82-4cd8-9cf1-def767ff9cd6.png)
+
+
+### 2. Soal 2 membuat navigation tanpa named routes
+Output soal 2 
+
+![soal2](https://user-images.githubusercontent.com/59384629/160361462-76394e24-3d14-47a7-bbf9-a19aae3070d4.png)
